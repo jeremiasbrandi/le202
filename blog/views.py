2598003e20b1d5c6e93b24dea572136ca35a4afa
Jeremias Brandi <jere@example.com>
2021-12-03 19:56:49 -0300

    Blog basico agregado

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Esta sera una pagina basica del blog.")
+
